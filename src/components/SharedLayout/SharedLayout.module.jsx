c0e74b68f35f859adb2faf825cc2a8c8f8dc93ce
Natali-Stankevych <rusakovanatali94@gmail.com>
2023-08-13 15:48:41 +0300

    add hw

---
+++ src/components/SharedLayout/SharedLayout.module.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  padding: 25px;
+`;
+export const Navigation = styled.nav`
+  margin-bottom: 24px;
+`;
+export const NavList = styled.ul`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 30px;
+
+  display: flex;
+  gap: 18px;
+  padding: 14px;
+
+  background: rgba(255, 255, 255, 0.1);
+  background-blend-mode: overlay;
+  backdrop-filter: blur(50px);
+  border-radius: 20px;
+`;
+
+export const NavItem = styled.li`
+  span {
+    color: #FA8072;
+  }
+
+  a:hover,
+  a:focus {
+    color: #FA8072;
+    span {
+      color: white;
+    }
+  }
+`;
